d33123fee9194a253a557ad4ac4f338f46ad4a00
Andre <andrepinedacunha@gmail.com>
2018-07-12 21:23:27 -0300

    Implementação de Bind e Factory

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,42 +7,23 @@ class NegociacaoController {
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
 
-        let self = this;
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia'
+        )
 
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-            get(target, prop, receiver){
-                if(["adiciona", "esvazia"].includes(prop) && typeof(target[prop]) == typeof(Function)){
-                    return function(){
-                        console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        self._negociacioesView.update(target);
-                    }                    
-                }
-                console.log("GIT");
-                
-                return Reflect.get(target, prop, receiver);
-            }
-        })
-
-        // this._listaNegociacoes = new ListaNegociacoes(model => 
-        //     this._negociacioesView.update(model));
-
-        this._negociacioesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacioesView.update(this._listaNegociacoes);
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($("#mensagemView"));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($("#mensagemView")),
+            'texto'
+        )
     }
 
     adiciona(event){
-
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-
         this._mensagem.texto = "Incluído com sucesso!";
-        this._mensagemView.update(this._mensagem);
-
         this._limpaFormulario();       
     }
 
@@ -62,9 +43,6 @@ class NegociacaoController {
 
     apaga(){
         this._listaNegociacoes.esvazia();
-
         this._mensagem.texto = "Lista apagada.";
-        this._mensagemView.update(this._mensagem);
-
     }
 }
