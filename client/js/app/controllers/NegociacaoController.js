2d3b7da0aa92ba2b849a24c1506de8757bc893e9
Andre <andrepinedacunha@gmail.com>
2018-07-14 11:41:48 -0300

    Aula 4

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -45,4 +45,17 @@ class NegociacaoController {
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = "Lista apagada.";
     }
+
+    importaNegociacoes(){
+        let service = new NegociacaoService();
+
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if(erro){
+                this._mensagem.texto = erro;
+                return;
+            }
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = "Negociações importadas com sucesso!";
+        });
+    }
 }
