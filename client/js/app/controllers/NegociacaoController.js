4f3a3b172e6995f7854ede891661499ed37bb002
Andre <andrepinedacunha@gmail.com>
2018-07-14 22:38:28 -0300

    Aula 5

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -48,14 +48,12 @@ class NegociacaoController {
 
     importaNegociacoes(){
         let service = new NegociacaoService();
-
-        service.obterNegociacoesDaSemana((erro, negociacoes) => {
-            if(erro){
-                this._mensagem.texto = erro;
-                return;
-            }
-            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = "Negociações importadas com sucesso!";
-        });
+        service
+            .obterNegociacoes()
+            .then(negociacoes => negociacoes.forEach(negociacao => {
+                    this._listaNegociacoes.adiciona(negociacao)
+                    this._mensagem.texto = "Negociações do período importadas com sucesso"
+            })).catch(erro => this._mensagem.texto = erro);
+       
     }
 }
