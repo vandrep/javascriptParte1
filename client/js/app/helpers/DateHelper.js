17b58e4d6420bf75110d9a00cf516c469bb93d79
Andre Pine da Cunha <andre.cunha@bb.com.br>
2018-07-17 13:39:15 -0300

    atividade 4 aula 6

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,9 +5,9 @@ class DateHelper{
 
     static textoParaData(texto){
 
-        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
-            throw new Error("Deve estar no formato aaaa-mm-dd");
-        return new Date(...texto.split('-').map((item,indice) => item - indice % 2));
+        if(!/\d{2}\/\d{2}\/\d{4}/.test(texto))
+            throw new Error("Deve estar no formato dd/mm/aaaa");
+        return new Date(...texto.split('/').reverse().map((item,indice) => item - indice % 2));
 
     }
 
