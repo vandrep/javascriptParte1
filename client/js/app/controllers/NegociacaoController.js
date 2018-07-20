27ad5a573e56f450ee3bbc0e8560eff0f0603776
Andre <andrepinedacunha@gmail.com>
2018-07-20 20:48:18 -0300

    JSIII Aula 1

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,11 +6,12 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._ordemAtual = '';
 
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes(),
             new NegociacoesView($('#negociacoesView')),
-            'adiciona', 'esvazia'
+            'adiciona', 'esvazia', 'ordena', 'inverteOrdem'
         )
 
         this._mensagem = new Bind(
@@ -55,5 +56,14 @@ class NegociacaoController {
                     this._mensagem.texto = "Negociações do período importadas com sucesso"
             })).catch(erro => this._mensagem.texto = erro);
        
+    }
+
+    ordena(coluna){
+        if(this._ordemAtual == coluna){
+            this._listaNegociacoes.inverteOrdem();
+        }else{
+            this._listaNegociacoes.ordena((a,b) => a[coluna] - b[coluna]);
+        }
+        this._ordemAtual = coluna;
     }
 }
